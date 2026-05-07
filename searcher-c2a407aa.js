cb57355389643fff0d6c877c99e6607b93ef5d90
simon zhao <chengbo.zhao@qq.com>
2026-05-07 12:04:09 +0900

    deploy: master 1510012 @ 2026-05-07 12:04

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8eb47a62.js',
+                path_to_root + 'searchindex-1b74ef73.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
